031c02b3dc4fa4f3a8f5541647a3d3bc33970737
Trond Weiseth <maatro_1001@hotmail.com>
2019-07-24 22:21:20 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import time
+name = input('Name: ')
+age = input('Age: ')
+year = (time.strftime('%Y'))
+birth = (int(year) - int(age))
+print()
+print('=======================================================')
+print("The Pation's name is: ' + name + ' and is ' + age + ' years old.")
+print('Year of birth:')
+print(int(birth))
+print('=======================================================')
